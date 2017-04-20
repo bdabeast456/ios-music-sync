387cc62342ed1df7e11021f4bf2f87dca1d29658
Brandon Pearl <brandonpearl@berkeley.edu>
2017-04-19 21:04:45 -0700

    project commit with youtube-helper library

---
+++ Music Sync/Music Sync/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Music Sync
+//
+//  Created by Brandon Pearl on 4/19/17.
+//  Copyright © 2017 Brandon Pearl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
